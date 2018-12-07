65ceb6f0ec572f8814f8eaaf1d89e886b0c59a56
Carl Smith <carlfsmithiii@gmail.com>
2018-12-07 13:53:54 -0500

    completed video 4

---
+++ src/Components/App.js
@@ -0,0 +1,57 @@
+import React, { Component, Fragment } from "react";
+import { Header, Footer } from "./Layouts/";
+import Exercises from "./Exercises";
+import { muscles, exercises } from "../store";
+
+export default class extends Component {
+  state = {
+    exercises,
+    exercise: {}
+  };
+
+  getExercisesByMuscles() {
+    return Object.entries(
+      this.state.exercises.reduce((exercises, exercise) => {
+        const { muscles } = exercise;
+        exercises[muscles] = exercises[muscles]
+          ? [...exercises[muscles], exercise]
+          : [exercise];
+        return exercises;
+      }, {})
+    );
+  }
+
+  handleCategorySelected = category => {
+    this.setState({
+      category
+    });
+  };
+
+  handleExerciseSelected = id => {
+    this.setState(({ exercise }) => ({
+      exercise: exercises.find(ex => ex.id === id)
+    }));
+  };
+
+  render() {
+    const exercises = this.getExercisesByMuscles(),
+      { category, exercise } = this.state;
+
+    return (
+      <Fragment>
+        <Header />
+        <Exercises
+          exercise={exercise}
+          category={category}
+          exercises={exercises}
+          onSelect={this.handleExerciseSelected}
+        />
+        <Footer
+          category={category}
+          muscles={muscles}
+          onSelect={this.handleCategorySelected}
+        />
+      </Fragment>
+    );
+  }
+}
